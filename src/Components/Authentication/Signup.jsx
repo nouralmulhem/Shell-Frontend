6991d39e03cb6044048f889a4c6366a636300afc
BasmaElhoseny <basmaelhoseny6@gmail.com>
2023-03-17 18:46:49 +0200

    Fixed Last Name Bug in sign up

--- src/Components/Authentication/Signup.jsx
+++ src/Components/Authentication/Signup.jsx
@@ -1,9 +1,11 @@
 import { Typography, TextField } from '@mui/material';
-// import axios from '../../services/instance'
+
+// Styles
 import axios from 'axios';
 import { useState } from 'react';
 import { ButtonSign, SignConatiner } from '../../styles/Signin';
 import { TypographyH3 } from '../../styles/SignupOverlay';
+import { NameTextField } from '../../styles/Signup';
 
 // Components
 import SnackBar from '../SnackBar';
@@ -11,7 +13,9 @@ import SocialAccounts from './SocialAccounts';
 
 function Signup({ btn2, condition }) {
   const [userName, setUserName] = useState('');
-  const [name, setName] = useState('');
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [password2, setPassword2] = useState('');
@@ -21,14 +25,13 @@ function Signup({ btn2, condition }) {
 
   const signUp = (e) => {
     e.preventDefault();
-    const [first, last] = name.split(' ');
     axios.post('https://cuert-backend-api.herokuapp.com/auth/register/', {
       username: userName,
       password,
       password2,
       email,
-      first_name: first,
-      last_name: last,
+      first_name: firstname,
+      last_name: lastname,
     })
       .then((response) => {
         console.log(response.data);
@@ -63,17 +66,32 @@ function Signup({ btn2, condition }) {
       <Typography variant="subtitle">
         or use your email for regrstration
       </Typography>
-      <TextField
-        label="Name"
-        type="text"
-        required
-        variant="standard"
-        size="small"
-        sx={{ width: '80%' }}
-        onChange={(e) => {
-          setName(e.target.value.trim());
-        }}
-      />
+      <NameTextField>
+        <TextField
+          label="First Name"
+          type="text"
+          required
+          variant="standard"
+          size="small"
+          sx={{ width: '40%' }}
+          onChange={(e) => {
+            setFirstName(e.target.value.trim());
+          }}
+        />
+        <TextField
+          label="Last Name"
+          type="text"
+          required
+          variant="standard"
+          size="small"
+          sx={{ width: '40%' }}
+          onChange={(e) => {
+            setLastName(e.target.value.trim());
+          }}
+        />
+
+      </NameTextField>
+
       <TextField
         label="Username"
         type="text"
---
+++ src/styles/Signup.js
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+import { styled } from '@mui/system';
+
+export const NameTextField = styled(Box)(() => ({
+  width: '80%',
+
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+}));
